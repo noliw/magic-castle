144df3285677d3504edfe5fe784d2d0fa1e3ed6b
noliw <noliw2017@gmail.com>
2022-09-02 19:14:20 +0200

    MERN-Stack Infrastructure

--- src/utilities/def weti(sentences):.py
+++
@@ -1,9 +0,0 @@
-def weti(sentences):
-    spaces=0
-    for i in range (len(sentences)):
-        if sentences[i] == " ":
-                spaces= spaces+1
-    return spaces
-
-
-print(weti(["alice and bob love leetcode i think so too this is great thanks very much"]))
